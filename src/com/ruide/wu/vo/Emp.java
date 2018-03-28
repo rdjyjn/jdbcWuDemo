58da3d9b2063e4b0ee34fbe15005861278998b30
rdjy <1805787596@qq.com>
2018-03-28 10:25:12 +0800

    测试

---
+++ src/com/ruide/wu/vo/Emp.java
@@ -0,0 +1,25 @@
+package com.ruide.wu.vo;
+
+public class Emp {
+	private int emono;
+	private String ename;
+	private String job;
+	public int getEmono() {
+		return emono;
+	}
+	public void setEmono(int emono) {
+		this.emono = emono;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+}
